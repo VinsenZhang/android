6f99d3c33022e18f760c9b4f8fbf2b9ba535d236
[VinsenZhang] <[m13687226890@163.com]>
2017-03-28 16:32:08 +0800

    ...

--- app/src/main/java/com/vinsen/application/MainActivity.java
+++ app/src/main/java/com/vinsen/application/MainActivity.java
@@ -1,37 +1,12 @@
 package com.vinsen.application;
 
-import android.support.annotation.UiThread;
-import android.support.annotation.WorkerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.vinsen.mylibrary.ThreadManager;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("vinsen");
-    }
 
-    private LinearLayout container;
-    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
