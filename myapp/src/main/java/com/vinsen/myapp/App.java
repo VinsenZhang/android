c1f175a75ebc27e32f6756cc60d00b1a58e72b8d
VinsenZhang <vinsen_zhang@163.com>
2018-08-11 12:19:09 +0800

    muti channel

--- myapp/src/main/java/com/vinsen/myapp/App.java
+++ myapp/src/main/java/com/vinsen/myapp/App.java
@@ -3,12 +3,21 @@ package com.vinsen.myapp;
 import android.app.Application;
 
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
 public class App extends Application {
 
+    private static Retrofit retrofit;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BuildConfig.domain)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
     }
 }
---
+++ myapp/src/main/java/com/vinsen/myapp/constants/AppURL.java
@@ -0,0 +1,22 @@
+package com.vinsen.myapp.constants;
+
+import com.vinsen.myapp.BuildConfig;
+
+public class AppURL {
+
+
+    private static final String domain = BuildConfig.domain;
+
+
+    private static final String home = domain + "/home";
+
+
+    private static final String home_banner = home + "/banner";
+
+
+
+
+
+
+
+}
